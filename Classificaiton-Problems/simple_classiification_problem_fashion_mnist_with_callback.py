792d7d2d893a3b87dd5f5bd2027a51b35779d52a
Prasaanth Selvakumar <prasaanthselvakumar@gmail.com>
2021-07-16 17:17:26 +0530

    callbacks intro for classification

---
+++ Classificaiton-Problems/simple_classiification_problem_fashion_mnist_with_callback.py
@@ -0,0 +1,31 @@
+from tensorflow import keras
+
+fm = keras.datasets.fashion_mnist
+(train_images, train_labels), (test_images, test_labels) = fm.load_data()
+
+
+class EpochEndCallback(keras.callbacks.Callback):
+
+    def on_epoch_end(self, epoch, logs=None):  # method gets called at the end of every epoch -
+        # check tf docs for more methods like this
+        if logs.get('accuracy', 0) > 0.85:
+            self.model.stop_training = True
+
+
+eeCallback = EpochEndCallback()
+
+model = keras.Sequential([keras.layers.Flatten(input_shape=(28, 28)),
+                          keras.layers.Dense(128, activation='relu'),
+                          keras.layers.Dense(10, activation='softmax')])
+model.compile(optimizer='adam', loss='sparse_categorical_crossentropy', metrics=['accuracy'])
+
+model.fit(train_images, train_labels, epochs=20, callbacks=[eeCallback], batch_size=256)
+
+model.evaluate(test_images, test_labels)
+"""
+The model evaluation will stop as soon as the target accuracy is achieved or 
+it will continue until the number of epochs are over
+This can be used to get models that satisfy our criteria
+
+On the 10th epoch it hits an accuracy of 85 percent on the training data   
+"""
